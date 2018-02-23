0399bf260f821ae5da1737d5696ac3458d388101
David Jirovec <david.jirovec@gmail.com>
2018-02-23 21:13:56 +0100

    Simplify using "outdated -r"

--- outdated-choco.ps1
+++ outdated-choco.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = "Stop"
 Import-Module -Name BurntToast
 
-$outdated = (choco outdated | Select-Object -Skip 4 | Select-String '^([^|]+)\|.*$').Matches.Groups | Where-Object {$_.Name -eq 1} | ForEach-Object {$_.Value}
+$outdated = (choco outdated -r | Select-String '^([^|]+)\|.*$').Matches.Groups | Where-Object {$_.Name -eq 1} | ForEach-Object {$_.Value}
 $pretty = ($outdated -join ', ')
 
 If ($outdated.count -gt 0) {
