423f240faaf2805cf8c4774b0cbd53ff3a68e307
David Jirovec <david.jirovec@gmail.com>
2023-09-05 07:13:02 +0200

    Ignore pinned

--- outdated-choco.ps1
+++ outdated-choco.ps1
@@ -1,7 +1,12 @@
 $ErrorActionPreference = "Stop"
 Import-Module -Name BurntToast
 
-$outdated = (choco outdated -r | Select-String '^([^|]+)\|.*$').Matches.Groups | Where-Object {$_.Name -eq 1} | ForEach-Object {$_.Value}
+# Get the list of pinned packages
+$pinnedPackages = (choco pin list -r | ForEach-Object { $_.Split('|')[0] }).Trim()
+
+# Get the list of outdated packages and filter out the pinned ones
+$outdated = (choco outdated -r | Select-String '^([^|]+)\|.*$').Matches.Groups | Where-Object {$_.Name -eq 1} | ForEach-Object {$_.Value} | Where-Object { $_ -notin $pinnedPackages }
+
 $pretty = ($outdated -join ', ')
 
 If ($outdated.count -gt 0) {
